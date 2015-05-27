ac0c8f91ab7285f745022fa93258403683de1fd9
Steven Edouard <steven.edouard1@gmail.com>
2015-05-26 18:51:01 -0700

    feat(api/trips) - implemented trips api

--- routes/trips.js
+++ routes/trips.js
@@ -1,60 +1,46 @@
 var express = require('express');
 var router = express.Router();
 var nconf = require('nconf');
+var tableUtils = require('../lib/table-utils');
 nconf.file({ file: './config.json' });
+
+var sourceUrl = nconf.get('host_protocol') + '://' + nconf.get('host_name') + ':' + nconf.get('host_port');
 
 /* GET home page. */
 router.get('/:id', function (req, res) {
-
-    if(!req.params.id) {
-        return res.send(400);
+  if(!req.params.id) {
+      return res.send(400);
+  }
+  
+  return tableUtils.getTripsForTripId(req.params.id)
+  .then(function(results){
+  
+    if(!results) {
+      res.send(404, 'could not find vehicle with id: ' + req.params.id);
     }
+  
+    var trips = [];
     
-    res.send(200, {
-    'data': [
-        {
-            'id': 'acf3ba91-8ee0-4b64-814a-52df6bdad442',
-            'type': 'trip',
-            'trip_events': [
-                {
-                    'id': '553ac599cb49af5c772f9983',
-                    'event_type': 'direction_change',
-                    'type': 'event',
-                    'timestamp': '2014-06-27T03:28:35 +07:00',
-                    'speed': 8,
-                    'location': {
-                        'id': '13412',
-                        'latitude': 56.649237,
-                        'longitude': 136.74491,
-                        'direction': 51,
-                        'altitude': 681
-                    }
-                },
-                {
-                    'id': '553ac599685144ba00e7f8cd',
-                    'event_type': 'engine_stop',
-                    'type': 'event',
-                    'timestamp': '2014-05-22T07:05:24 +07:00',
-                    'speed': 3,
-                    'location': {
-                        'id': '13413',
-                        'latitude': -21.687184,
-                        'longitude': -77.175881,
-                        'direction': 158,
-                        'altitude': 9
-                    }
-                }
-            ],
-            'links': {
-                'self': 'http://localhost:3000/vehicles/553ac59934dff597a9708c71/trip/acf3ba91-8ee0-4b64-814a-52df6bdad442',
-                'driver': {
-                    'related': 'http://localhost:3000/drivers/acf3ba91-8ee0-4b64-814a-52df6bdad442'
-                }
-            }
+    results.forEach(function(result){
+      result.links = {
+        self: sourceUrl + '/trips' + '/' + req.params.id,
+        driver: {
+          related: sourceUrl + '/drivers/' + result.driver_id
         }
-    ]
+      };
+      delete result.driver_id;
+      trips.push(result);
+    });
+      
+    res.send({ data: trips });
+  })
+  .catch(function(err) {
+      console.error('ERROR:');
+      console.dir(err);
+      return res.send(500, err);
+  });
 });
-});
+
 router.get('/:id/driver', function (req, res) {
 
     if(!req.params.id) {
